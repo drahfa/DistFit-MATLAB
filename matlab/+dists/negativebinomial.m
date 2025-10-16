af99cb7f0d6a20fb6b339ce2e5ff94363181f666
Dr. Faisal Ayob <ahmadfaisal9@yahoo.com>
2025-10-16 15:18:04 +0800

    Bugfix negativebinomial.m

--- matlab/+dists/negativebinomial.m
+++ matlab/+dists/negativebinomial.m
@@ -13,16 +13,44 @@ m.rnd = @(n,th) nbinrnd(max(th(1),eps), min(max(th(2),eps),1-eps), n, 1);
 
     function th = fitf(x, th0)
         x = x(:); x = x(isfinite(x) & x>=0);
-        if nargin<2 || isempty(th0), th0 = m.start(x); end
-        try
-            phat = mle(x, 'pdf', @(y,r,p) nbinpdf_local(y,r,p), 'start', th0, 'lowerbound',[eps eps], 'upperbound',[Inf 1-eps]);
-            th = phat(:).';
-        catch
-            nll = @(p) -sum(log(max(nbinpdf_local(x, max(p(1),eps), min(max(p(2),eps),1-eps)), realmin)));
-            opts = optimset('display','off');
-            p = fminsearch(nll, th0, opts);
-            th = [max(p(1),eps), min(max(p(2),eps),1-eps)];
+        if isempty(x)
+            th = [1, 0.5];
+            return;
         end
+        if nargin<2 || isempty(th0), th0 = m.start(x); end
+        th0 = enforce_bounds(th0);
+        phi0 = theta_to_phi(th0);
+        opts = optimset('display','off','tolx',1e-8,'tolfun',1e-8,'maxiter',4000,'maxfunevals',8000);
+        [phi, ~, exitflag] = fminsearch(@(ph) nll_phi(ph, x), phi0, opts);
+        if exitflag <= 0
+            % Retry from a variance-inflated guess if needed.
+            alt = enforce_bounds([max(th0(1)*2, eps), min(max((th0(2)+0.5)/2, eps), 1-eps)]);
+            phi = theta_to_phi(alt);
+            [phi, ~] = fminsearch(@(ph) nll_phi(ph, x), phi, opts);
+        end
+        th = phi_to_theta(phi);
+    end
+
+    function phi = theta_to_phi(theta)
+        r = max(theta(1), eps);
+        p = min(max(theta(2), eps), 1-eps);
+        phi = [log(r); log(p/(1-p))];
+    end
+
+    function theta = phi_to_theta(phi)
+        r = max(exp(phi(1)), eps);
+        p = 1./(1+exp(-phi(2)));
+        p = min(max(p, eps), 1-eps);
+        theta = [r, p];
+    end
+
+    function val = nll_phi(phi, data)
+        th = phi_to_theta(phi);
+        val = -sum(log(max(nbinpdf_local(data, th(1), th(2)), realmin)));
+    end
+
+    function out = enforce_bounds(theta)
+        out = [max(theta(1), eps), min(max(theta(2), eps), 1-eps)];
     end
 end
 
@@ -48,4 +76,3 @@ function F = nbincdf_local(k,r,pp)
 kk = floor(k);
 F = betainc(pp, r, kk+1);
 end
-
